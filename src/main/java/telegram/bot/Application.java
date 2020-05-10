b1ac19169fdff193b2d9d8c8bd9213941b7d8d3c
MrazotaOne <MrazotaOne@Computer>
2020-05-10 22:06:41 +0400

    test

--- src/main/java/telegram/bot/Application.java
+++ src/main/java/telegram/bot/Application.java
@@ -25,12 +25,28 @@ package telegram.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication()
-public class Application {
+@RestController
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
+    @RequestMapping(value = "/kek")
+    public String demo() {
+        return "hello";
+
     }
 
 }
